06ecac480d9fbc522562c8bfb92a0151cfe54227
Eyan2005 <91119222+Eyan2005@users.noreply.github.com>
2022-09-19 11:06:44 +0400

    Update CalendarViewHolder.java

--- CalendarViewHolder.java
+++ CalendarViewHolder.java
@@ -6,20 +6,30 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    private final ArrayList<LocalDate> days;
+    public final View parentView;
     public final TextView dayOfMonth;
     private final CalendarAdapter.OnItemListener onItemListener;
-
-    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener) {
+    public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener, ArrayList<LocalDate> days)
+    {
         super(itemView);
+        parentView = itemView.findViewById(R.id.parentView);
         dayOfMonth = itemView.findViewById(R.id.cellDayText);
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
-    }
+        this.days = days;
+    } //to display the calendar views when the client opens
+    //the app or scrolls to another month, week or day and
+    //how it should change when interacted with
 
     @Override
-    public void onClick(View view) {
-        onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
-    }
+    public void onClick(View view)
+    {
+        onItemListener.onItemClick(getAdapterPosition(), days.get(getAdapterPosition()));
+    } //to retrieve the adapter, which binds calendar views with the data they request
 }
