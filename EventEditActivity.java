bb306a90554bdf915584fb9a70f6b2a064d74132
Eyan2005 <91119222+Eyan2005@users.noreply.github.com>
2022-09-19 11:10:03 +0400

    Create EventEditActivity.java

---
+++ EventEditActivity.java
@@ -0,0 +1,45 @@
+package com.example.therapycalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.time.LocalTime;
+
+public class EventEditActivity extends AppCompatActivity
+{
+    private EditText eventNameET;
+    private TextView eventDateTV, eventTimeTV;
+
+    private LocalTime time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_edit);
+        initWidgets();
+        time = LocalTime.now();
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+    }//to display the selected date and current time using text view from the recyclerview library
+
+    private void initWidgets()
+    {
+        eventNameET = findViewById(R.id.eventNameET);
+        eventDateTV = findViewById(R.id.eventDateTV);
+        eventTimeTV = findViewById(R.id.eventTimeTV);
+    }//to look for the events by searching for their IDs
+
+    public void saveEventAction(View view)
+    {
+        String eventName = eventNameET.getText().toString();
+        Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time);
+        Event.eventsList.add(newEvent);
+        finish();
+    }//when event details are selected and client presses the save button, a new event
+    //object is instantiated and added to the event list
+}
