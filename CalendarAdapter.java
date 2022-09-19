4f570bfb89f5c71e0297aa8ed9b165b627541854
Eyan2005 <91119222+Eyan2005@users.noreply.github.com>
2022-09-19 11:05:40 +0400

    Update CalendarAdapter.java

--- CalendarAdapter.java
+++ CalendarAdapter.java
@@ -1,5 +1,6 @@
 package com.example.therapycalendar;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,40 +8,61 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-
-public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
+//extension of recyclerview.adapter to bind calendar views to their data
+class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
 {
-    private final ArrayList<String> daysOfMonth;
+    private final ArrayList<LocalDate> days;
     private final OnItemListener onItemListener;
 
-    public CalendarAdapter(ArrayList<String> daysOfMonth, OnItemListener onItemListener) {
-        this.daysOfMonth = daysOfMonth;
+    public CalendarAdapter(ArrayList<LocalDate> days, OnItemListener onItemListener)
+    {
+        this.days = days;
         this.onItemListener = onItemListener;
-    }
+    } //constructor for arraylist of days and the attribute that detects clicks on dates
 
     @NonNull
     @Override
-    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.calendar_cell, parent, false);
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-        layoutParams.height = (int) (parent.getHeight() * 0.1666666666);
-        return new CalendarViewHolder(view, onItemListener);
-    }
+        if(days.size() > 15) //for the month view
+            layoutParams.height = (int) (parent.getHeight() * 0.166666666);
+        else //for the week view
+            layoutParams.height = parent.getHeight();
+
+        return new CalendarViewHolder(view, onItemListener, days);
+    } //so that each view in the calendar view group is aligned
+    //vertically and takes up 1/6 of the total space
 
     @Override
-    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
-        holder.dayOfMonth.setText(daysOfMonth.get(position));
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position)
+    {
+        final LocalDate date = days.get(position);
 
-    }
+        holder.dayOfMonth.setText(String.valueOf(date.getDayOfMonth()));
+
+        if(date.equals(CalendarUtils.selectedDate))
+            holder.parentView.setBackgroundColor(Color.LTGRAY);
+
+        if(date.getMonth().equals(CalendarUtils.selectedDate.getMonth()))
+            holder.dayOfMonth.setTextColor(Color.BLACK);
+        else
+            holder.dayOfMonth.setTextColor(Color.LTGRAY);
+    } //to display dates on the view where they need to
+    //be in which color and which color they should switch to when selected
 
     @Override
-    public int getItemCount() {
-        return daysOfMonth.size();
+    public int getItemCount()
+    {
+        return days.size();
     }
 
-    public interface OnItemListener {
-        void onItemClick(int position, String dayText);
-    }
+    public interface  OnItemListener
+    {
+        void onItemClick(int position, LocalDate date);
+    } //for when dates are selected
 }
